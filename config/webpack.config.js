c62d3a49f84f2e83af9d270be77a8da74cfb61dc
Chris Sancho <sanchochris@gmail.com>
2017-06-09 13:10:28 -0400

    remove sass & add author taxonomy

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,13 +10,11 @@ const definePlugin = new webpack.DefinePlugin({
   }
 })
 
-let extractSCSS = new ExtractTextPlugin('css/[name].css'/*, {allChunks: true}*/);
 let extractLESS = new ExtractTextPlugin('css/[name]-less.css'/*, {allChunks: true}*/);
 
 
 module.exports = {
     entry: [
-        __dirname + '/../src/sass/style.scss',
         __dirname + '/../src/less/importer.less',
         __dirname + '/../src/js/main.js',
     ],
@@ -39,21 +37,12 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
-                loader: extractSCSS.extract({
-                fallback: 'style-loader',
-                //resolve-url-loader may be chained before sass-loader if necessary
-                use: ['css-loader', 'sass-loader']
-              })
-            },
-            {
               test: /\.less$/i,
               use: extractLESS.extract([ 'css-loader', 'less-loader' ])
             }
         ]
     },
     plugins: [
-        extractSCSS,
         extractLESS,
         new webpack.ProvidePlugin({
             Promise: 'imports?this=>global!exports?global.Promise!es6-promise',
