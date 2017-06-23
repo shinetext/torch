c92ade1e7f124ddffbf1a58eb13d0fca52d9e667
Chris Sancho <sanchochris@gmail.com>
2017-06-23 17:14:56 -0400

    fix attempt#1

--- hugo/lib/getUrl.js
+++ hugo/lib/getUrl.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 */
 // baseURL = "https://advice-staging.shinetext.com"
 
-const hugoConfigBase = fs.readFileSync(__dirname + '/../../config/hugo.config.base.toml').toString();
+const hugoConfigBase = fs.readFileSync(__dirname + '/../../config/hugo.config.base.toml');
 const baseURL = process.env.BASE_URL ? `baseURL = "${process.env.BASE_URL}"\n` //format baseURL for toml file
                 : `baseURL = "\/"\n`;
 
-const hugoConfig = baseURL + hugoConfigBase;
+const hugoConfig = baseURL + hugoConfigBase.toString();
 
 // Write hugo config file to config folder
 fs.writeFileSync(__dirname + '/../../config/hugo.config.toml', hugoConfig, function(err) {
