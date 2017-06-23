eef455ab937af595a260a5c5d338d89ea55bdc40
Chris Sancho <sanchochris@gmail.com>
2017-06-23 16:41:04 -0400

    add script to write baseURL to hugo config file

---
+++ hugo/lib/getUrl.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+/**
+  * Get the current base for the hugo config and baseURL from Netlify ENV
+  * prepend baseURL either staging or production URL to hugo config base
+*/
+// baseURL = "https://advice-staging.shinetext.com"
+
+const hugoConfigBase = fs.readFileSync(__dirname + '/../../config/hugo.config.base.toml').toString();
+const baseURL = process.env.BASE_URL ? `baseURL = "${process.env.BASE_URL}"\n` //format baseURL for toml file
+                : `baseURL = "\/"\n`;
+
+const hugoConfig = baseURL + hugoConfigBase;
+
+// Write hugo config file to config folder
+fs.writeFileSync(__dirname + '/../../config/hugo.config.toml', hugoConfig, function(err) {
+  if (err) {
+    console.log(err)
+  }
+});
