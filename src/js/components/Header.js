b8ac35b35f67cbb42db92d6660d06b9988310813
Chris Sancho <sanchochris@gmail.com>
2017-06-27 10:46:45 -0400

    add missing ga events to header and join bar

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
@@ -6,7 +6,6 @@ class Header extends Component {
   componentDidMount() {
     $('#mobile-menu-button').click(this.toggleMobileMenu);
     $('#mobile-menu-button-close').click(this.toggleMobileMenu);
-
   }
   toggleMobileMenu() {
     $('body').toggleClass('mobile-menu-open');
@@ -18,7 +17,10 @@ class Header extends Component {
       <header>
         <div className="navbar">
           <a href="http://www.shinetext.com">
-            <img className="shine-logo" src="https://images.contentful.com/awpxl2koull4/FFhnW3pbga4qmCOM24kmc/8c718dac9a5357db86c55b637461f4f3/Shine-logo-web.png?w=100"/>
+            <img
+              className="shine-logo"
+              src="https://images.contentful.com/awpxl2koull4/FFhnW3pbga4qmCOM24kmc/8c718dac9a5357db86c55b637461f4f3/Shine-logo-web.png?w=100"
+            />
           </a>
 
           <div className="navbar-links-container">
@@ -32,10 +34,26 @@ class Header extends Component {
               <a href="http://www.shinetext.com/squad">The Squad</a>
             </div>
           </div>
-          <i id="mobile-menu-button" className="fa fa-bars" aria-hidden="true"></i>
+          <i
+            id="mobile-menu-button"
+            className="fa fa-bars"
+            aria-hidden="true"
+            ga-on="click"
+            ga-event-category="Behavior"
+            ga-event-action="mobile menu click"
+            ga-event-label="open"
+          />
         </div>
         <div id="mobile-menu">
-          <i id="mobile-menu-button-close" className="fa fa-times" aria-hidden="true"></i>
+          <i
+            id="mobile-menu-button-close"
+            className="fa fa-times"
+            aria-hidden="true"
+            ga-on="click"
+            ga-event-category="Behavior"
+            ga-event-action="mobile menu click"
+            ga-event-label="close"
+          />
           <ul>
             <li>
               <a href="/">Get Advice</a>
@@ -49,9 +67,8 @@ class Header extends Component {
           </ul>
         </div>
       </header>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <Header/>, document.getElementById('header'));
+ReactDOM.render(<Header />, document.getElementById('header'));
